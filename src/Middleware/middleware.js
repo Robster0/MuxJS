05df7fc2202b29083fa661ebae23c039286673a5
Robster <robbom0319@gmail.com>
2022-11-03 02:29:06 +0100

    Middleware class

--- src/Middleware/middleware.js
+++ src/Middleware/middleware.js
@@ -1,29 +1,35 @@
+const { undefinedObj, substr } = require('../Parse/helpers')
+
 class Middleware {
 
     #currExecutableMW
+    #socket
 
-    constructor(mw, path = null, handler = null) {
+    constructor(mw, sock, path = null, handler = null) {
+
+        this.#socket = sock;
+
         if(path === null) {
             this.#currExecutableMW = mw;
         } else {
-            this.count = 0;       
-            this.#load(mw, path, handler)
+            this.#load(mw, path)
+            this.#currExecutableMW.push(handler);
         }
     }
 
-    #load(mw, path, handler) {
+    #load(mw, path) {
         try
         {
             this.#currExecutableMW = [];
 
             for(let i = 0; i<mw.length; i++) {
-                if(mw[i].path === path || mw[i].path === '*' || this.#subroutemw(path, mw[i].path)) {
+                if(mw[i].path === path ||
+                   mw[i].path.length === 0 ||
+                   substr(path, 0, mw[i].path.length) === mw[i].path) {
+                    
                     this.#currExecutableMW.push(mw[i].middleware);
                 }
             }
-    
-            this.#currExecutableMW.push(handler);
-            this.count = this.#currExecutableMW.length - 1;
         }
         catch(err)
         {
@@ -31,47 +37,45 @@ class Middleware {
         }
     }
 
-    #subroutemw(path, mwPath) {
-        try
-        {
-            if(mwPath[0] === '*' || mwPath[mwPath.length - 1] !== '*') return false;
-
-
-            for(let i = 0; i<mwPath.length - 1; i++)
-            {
-                if(path[i] !== mwPath[i]) return false;
-            }
-
-            return true;
-        }
-        catch(err)
-        {
-            return false;
-        }
-    }
-
-     /**
-     * Serves the next handler or middleware
-     * @param {w} ResponseWriter Passes down the response writer variable to the next handler
-     * @param {r} Request Passes down the request variable to the next handler
-     */
+    /**
+    * Serves the next middleware or handler
+    * @param {class} w Passes down the response writer variable to the next handler
+    * @param {class} r Passes down the request variable to the next handler
+    */
     Serve(w, r) {
         try
         {
-           if(this.#currExecutableMW.length < 1) throw new Error("No executable middlewares")
+            if(this.#currExecutableMW.length < 1) throw new Error("No executable middlewares")
  
-           const next = this.#currExecutableMW[0];
-           let updated = []
+            if(!this.#socket.writable) return;
  
-           for(let i = 1; i<this.#currExecutableMW.length; i++)
-           {
-                updated.push(this.#currExecutableMW[i]);
-           }
+            const nextMw = this.#currExecutableMW[0];
+            let mwList = []
 
-           if(updated.length === 0)
-                next !== null && next(w, r)
-            else 
-                next(w, r, new Middleware(updated))
+            for(let i = 1; i<this.#currExecutableMW.length; i++)
+            {
+                mwList.push(this.#currExecutableMW[i]);
+            }
+
+            if(mwList.length !== 0) 
+                return nextMw(w, r, new Middleware(mwList, this.#socket))
+    
+            if(undefinedObj(r.Body) && undefinedObj(r.Files)) {
+                const ct = r.Headers['Content-Type']
+
+                if(ct === 'application/json' || ct === 'application/x-www-form-urlencoded') {
+                    r.Body = r.Raw.toString('utf-8')
+                } else {
+                    r.Files['File'] = {
+                        data: r.Raw,
+                        type: 'Unknown',
+                        size: r.Raw?.length
+                    }
+                }
+            }  
+
+
+            nextMw(w, r)
         }
         catch(err)
         {
